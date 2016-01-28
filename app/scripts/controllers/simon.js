743244ea2afbef1e7dd003c8af85582c23c09ddd
hectorlambies <hectorlambies@gmail.com>
2016-01-28 19:46:04 +0100

    mas cosas

--- app/scripts/controllers/simon.js
+++ app/scripts/controllers/simon.js
@@ -9,14 +9,25 @@
  */
 angular.module('simonApp')
   .controller('SimonCtrl', function ($scope) {
-  	$scope.colores = [];
   	$scope.titulo = 'simon'
+    $scope.acumuladorverde = 0;
+    $scope.acumuladorrojo = 0;
+    $scope.acumuladoramarillo = 0;
+    $scope.acumuladorazul = 0;
   	$scope.play = function(color){
+      if(color=='verde'){$scope.acumuladorverde = $scope.acumuladorverde +1;}
+      if(color=='rojo'){$scope.acumuladorrojo = $scope.acumuladorrojo +1;;}
+      if(color=='amarillo'){$scope.acumuladoramarillo = $scope.acumuladoramarillo +1;;}
+      if(color=='azul'){$scope.acumuladorazul = $scope.acumuladorazul +1;;}
   		$scope.colores.push(color);
   	}
-  	$scope.limpiar = function(clear){
-  		$scope.colores = [];
+  	$scope.limpiar = function(){
+      $scope.acumuladorverde = 0;
+      $scope.acumuladorrojo = 0;
+      $scope.acumuladoramarillo = 0;
+      $scope.acumuladorazul = 0;
   	}
+
   	
 			
 			
